4fc607a60ff963ba948e0e7f348bc154641248cf
Ashvan Wal <35784480+AshvanWal@users.noreply.github.com>
2020-05-28 14:20:32 -0700

    Committed an error

--- lab_3/app/index.js
+++ lab_3/app/index.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(express.static("public"));
 
 app.get("/test", (req, res) => {
-  res.status(200).send("Hello, World!");
+  res.status(200).send("Hello, Ashvan!");
 });
 
 module.exports = app.listen(port, () => {
